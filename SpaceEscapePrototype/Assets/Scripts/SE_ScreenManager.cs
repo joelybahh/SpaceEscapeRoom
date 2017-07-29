0374bca7fdbd25994b39be3f1fdd5140c26d7ad0
Joel Gabriel <joelybahh@gmail.com>
2017-07-29 10:17:25 +1000

    Update to SE_ScreenManager
    updated variable name

--- SpaceEscapePrototype/Assets/Scripts/SE_ScreenManager.cs
+++ SpaceEscapePrototype/Assets/Scripts/SE_ScreenManager.cs
@@ -4,24 +4,24 @@ using UnityEngine;
 
 public class SE_ScreenManager : MonoBehaviour {
 
-    public List<GameObject> tabletScreens;
+    public List<GameObject> m_tabletScreens;
 	
 	void Update () {
         switch (SE_TabletScreenState.GetCurState()) {
             case SE_TabletScreenState.eScreenState.LOCK_SCREEN: {
-                    if (tabletScreens[0].activeSelf == true) return;
+                    if (m_tabletScreens[0].activeSelf == true) return;
                     else {
-                        tabletScreens[0].SetActive(true);
-                        tabletScreens[1].SetActive(false);
+                        m_tabletScreens[0].SetActive(true);
+                        m_tabletScreens[1].SetActive(false);
                     }
                     break;
                 }
             case SE_TabletScreenState.eScreenState.HOME_SCREEN: {
-                    if (tabletScreens[1].activeSelf == true) return;
+                    if (m_tabletScreens[1].activeSelf == true) return;
                     else
                     {
-                        tabletScreens[1].SetActive(true);
-                        tabletScreens[0].SetActive(false);
+                        m_tabletScreens[1].SetActive(true);
+                        m_tabletScreens[0].SetActive(false);
                     }
                     break;
                 }
