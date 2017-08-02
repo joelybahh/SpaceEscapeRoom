40a2cf2a4bc472d92b4bd48d12e9fad2fbdac141
Joel Gabriel <joelybahh@gmail.com>
2017-08-02 18:02:57 +1000

    Added Animation Logic for door opening

--- SpaceEscapePrototype/Assets/Scripts/Gameplay/SE_PuzzleEyeScanner.cs
+++ SpaceEscapePrototype/Assets/Scripts/Gameplay/SE_PuzzleEyeScanner.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Animations;
 using UnityEngine;
 
 public class SE_PuzzleEyeScanner : SE_PuzzleEvent {
     [SerializeField]
-    ParticleSystem eyeScanner;
-
+    ParticleSystem m_eyeScanner;
+    [SerializeField]
+    Animator m_controller; 
     AudioClip m_announcement;
     bool hasCompleted = false;
     float scanTimer = 0;
@@ -17,7 +19,7 @@ public class SE_PuzzleEyeScanner : SE_PuzzleEvent {
 
 
     private void OnTriggerEnter(Collider other) {
-        eyeScanner.Play();
+        m_eyeScanner.Play();
 
         if (other.tag == "EyeKey") {
             m_completed = true;
@@ -29,13 +31,14 @@ public class SE_PuzzleEyeScanner : SE_PuzzleEvent {
             scanTimer += Time.deltaTime;
             if(scanTimer > 2.0f) {
                 m_completed = true;
+                m_controller.SetTrigger("HasScannedEye");
             }
         }
 
     }
 
     private void OnTriggerExit(Collider other) {
-        eyeScanner.Stop();
+        m_eyeScanner.Stop();
         if (other.tag == "EyeKey") {
             scanTimer = 0;
         }
