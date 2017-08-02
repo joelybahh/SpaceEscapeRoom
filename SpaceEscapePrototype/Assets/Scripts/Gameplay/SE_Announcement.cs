685b2956e1077dc1ea89763154ec951535a22760
Joel Gabriel <joelybahh@gmail.com>
2017-08-02 10:28:11 +1000

    #18 fixed the Announcer trying to access private variables by adding getters and setters

--- SpaceEscapePrototype/Assets/Scripts/Gameplay/SE_Announcement.cs
+++ SpaceEscapePrototype/Assets/Scripts/Gameplay/SE_Announcement.cs
@@ -5,14 +5,21 @@ using UnityEngine.Events;
 
 public class SE_Announcement : MonoBehaviour {
     [Header("Time to trigger announcement")]
-    [SerializeField] public float m_minutes;
-    [SerializeField] public float m_seconds;
+    [SerializeField] private float m_minutes;
+    [SerializeField] private float m_seconds;
 
     [Header("Announcement to play")]
-    [SerializeField] public AudioClip m_announcmentAudio;
+    [SerializeField] private AudioClip m_announcmentAudio;
 
     [Header("Announcement Events")]
-    [SerializeField]
-    private UnityEvent m_OnAnnounceStart;
-    private UnityEvent m_OnAnnounceEnd;
+    [SerializeField] private UnityEvent m_OnAnnounceStart;
+    [SerializeField] private UnityEvent m_OnAnnounceEnd;
+
+    #region Getters
+
+    public float Minutes { get { return m_minutes; } }
+    public float Seconds { get { return m_seconds; } }
+    public AudioClip AnnouncementAudio { get { return m_announcmentAudio; } }
+
+    #endregion
 }
