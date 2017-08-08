a167fd285c4f9c401c8a92101e4c2350a6fc0809
MatthewJ97 <matthewjones1256@gmail.com>
2017-08-08 13:16:00 +1000

    #13 Added functions to change the tag of the floor to be locked or unlocked

---
+++ SpaceEscapePrototype/Assets/Scripts/Gameplay/SE_Floor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SE_Floor : MonoBehaviour {
+
+	void UnlockFloor( ) {
+        tag = "UnlockedFloor";
+    }
+
+    void LockFloor( ) {
+        tag = "LockedFloor";
+    }
+}
